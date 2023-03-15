3daef69402b61381cdfae19eef5992d68c2d229b
“Jon <jon.navarro@estudiants.urv.cat>
2023-03-15 09:03:29 +0100

    cambios

--- LB_server.py
+++ LB_server.py
@@ -1,6 +1,7 @@
 
 import grpc
 from concurrent import futures
+import time
 from datetime import datetime
 from dateutil import parser
 import LB_server_pb2
@@ -9,13 +10,15 @@ import LB_server_pb2_grpc
 from LB_service import LB_service
 class LB_ServiceServicer(LB_server_pb2_grpc.LB_ServiceServicer):
     def send_meteo_data(self, data, context):
-        response = LB_service.send_meteo_data(self, data)
-        #Puede ser que haya que hacer con el proto (pb2) para actualizar los mensajes de vuelta al cliente
+        # Hay que pasarle el parametro data separando las variables o junto?
+        LB_service.send_meteo_data(data.temperature, data.humidity, data.datetime)
+        response = LB_Server_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
         return response
-    def send_pollution_data(self,data,context):
-        response = LB_service.send_pollution_data(self, data)
-        #Puede ser que haya que hacer con el proto (pb2) para actualizar los mensajes de vuelta al cliente
+    def send_pollution_data(self, data, context):
+        LB_service.send_pollution_data(self, data.co2, data.datetime)
+        response = LB_Server_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
         return response
+
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 LB_server_pb2_grpc.add_LB_serviceServicer_to_server(LB_ServiceServicer(), server)
 print('Starting server. Listening on port 50051.')
--- LB_service.py
+++ LB_service.py
@@ -1,9 +1,20 @@
 import meteo_utils
 class LB_service:
-    def send_meteo_data(self, request):
-        air = meteo_utils.MeteoDataProcessor()
-        processed_data = air.process_meteo_data(self, request)
-    def send_pollution_data(self, request):
+    def __init__(self):
+        self.servers = [
+            ('192.168.1.1', '50051'),
+            ('192.168.1.2', '50051'),
+            ('192.168.1.3', '50051')
+        ]
+        self.server_index = 0
+    def send_meteo_data(self, temperature, humidity, time):
+        # Crear cliente
+        # LLamar a la RPC del proto del server
+        # Round Robin
+        server = self.servers[self.server_index]
+        self.server_index = (self.server_index + 1) % len(self.servers)
+        # LLamar a process_meteo_data
+    def send_pollution_data(self, co2, time):
         pollution = meteo_utils.MeteoDataProcessor()
         processed_pollution = pollution.process_pollution_data(self, request)
 
