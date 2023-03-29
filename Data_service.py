f23fcd396700a92dc774d7cc450de90fe9f98ba3
tomascan <tomascan99@gmail.com>
2023-03-29 20:37:19 +0200

    Primera Parte

--- Data_service.py
+++ Data_service.py
@@ -11,9 +11,20 @@ class Data_service:
         dt = datetime.datetime.fromtimestamp(meteo.datetime.ToSeconds()) 
         print(dt.strftime("%Y-%m-%d %H:%M:%S"))
         
+        air_wellness = meteo_utils.MeteoDataProcessor()
+        wellness = air_wellness.process_meteo_data(meteo)
+        print(wellness)
+        
     def process_pollution_data(self, pollut):
         # REDIS ->  put (pollution)
-        print('He llegau')
+        print('Pollution: ', pollut.co2)
+        
+        dt = datetime.datetime.fromtimestamp(pollut.datetime.ToSeconds()) 
+        print(dt.strftime("%Y-%m-%d %H:%M:%S"))
+        
+        air_pollution = meteo_utils.MeteoDataProcessor()
+        pollution = air_pollution.process_pollution_data(pollut)
+        print(pollution)
 
 Data_service = Data_service()
 
