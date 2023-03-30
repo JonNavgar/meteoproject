cb5dd03885df153b3d6c46963f4fcbb5b0e90977
tomascan <tomascan99@gmail.com>
2023-03-30 17:13:57 +0200

    Redis

--- Data_server.py
+++ Data_server.py
@@ -8,10 +8,12 @@ import Data_server_pb2
 import Data_server_pb2_grpc
 
 
+
 from Data_service import Data_service
 class Data_serviceServicer(Data_server_pb2_grpc.Data_serviceServicer):
 
     def process_meteo_data(self, data, context):
+    
         Data_service.process_meteo_data(data)
         response = Data_server_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
         return response
@@ -23,6 +25,7 @@ class Data_serviceServicer(Data_server_pb2_grpc.Data_serviceServicer):
 
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 Data_server_pb2_grpc.add_Data_serviceServicer_to_server(Data_serviceServicer(), server)
+
 print('Listening')
 server.add_insecure_port('0.0.0.0:50054')
 server.add_insecure_port('0.0.0.0:50055')
--- Data_service.py
+++ Data_service.py
@@ -1,30 +1,42 @@
 import meteo_utils
 import datetime
 from google.protobuf.timestamp_pb2 import Timestamp
+import redis
 
+
+redis_host = "localhost"
+redis_port = 6379
+redis_password = ""
+
+r_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
+        
 
 class Data_service:
     def process_meteo_data(self, meteo):
-        # REDIS -> put (wellness)
-        print("Temperatura:", meteo.temperature, "Humedad:", meteo.humidity)
+        #print("Temperatura:", meteo.temperature, "Humedad:", meteo.humidity)
         
         dt = datetime.datetime.fromtimestamp(meteo.datetime.ToSeconds()) 
-        print(dt.strftime("%Y-%m-%d %H:%M:%S"))
         
         air_wellness = meteo_utils.MeteoDataProcessor()
         wellness = air_wellness.process_meteo_data(meteo)
-        print(wellness)
+        
+        key = dt.strftime("%Y-%m-%d %H:%M:%S")
+        r_client.set(key, wellness)
+        #redis-cli en terminal y keys * 
+        
+
         
     def process_pollution_data(self, pollut):
-        # REDIS ->  put (pollution)
-        print('Pollution: ', pollut.co2)
         
         dt = datetime.datetime.fromtimestamp(pollut.datetime.ToSeconds()) 
-        print(dt.strftime("%Y-%m-%d %H:%M:%S"))
         
         air_pollution = meteo_utils.MeteoDataProcessor()
         pollution = air_pollution.process_pollution_data(pollut)
-        print(pollution)
+        
+        key = dt.strftime("%Y-%m-%d %H:%M:%S")
+        r_client.set(key,pollution)
+
+
 
 Data_service = Data_service()
 
